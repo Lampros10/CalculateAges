07a6192bc1d32057033aa3a4605c3a42456e5a3c
Lampros10 <58770805+Lampros10@users.noreply.github.com>
2020-07-03 17:39:14 +0300

    Add files via upload

---
+++ CalculateAges.java
@@ -0,0 +1,32 @@
+public class CalculateAges {
+
+	public static void main (String [] args) {
+		int CurrentYear = 2020;
+		int MyAge = 26;
+		int NikosAge = 12;
+		int GeorgeAge = 52;
+		int MariaAge = 72;
+		int PastYear = 1960;
+		int FutureYear = 2040;
+		int[] arr = new int[4];
+		arr[0] = MyAge;
+		arr[1] = NikosAge;
+		arr[2] = GeorgeAge;
+		arr[3] = MariaAge;
+		for (int i = 0; i < 4; i++) {
+			System.out.println(CurrentYear - arr[i]);
+		}
+		
+		for (int j = 0; j < 4; j++) {
+			System.out.println(PastYear - arr[j]);
+		}
+
+		for (int k = 0; k < 4; k++) {
+			System.out.println(FutureYear - arr[k]);
+		}
+
+
+}
+
+
+}
